30ec6a5f11514fa2aab0c6daf4aadadf4476cef1
Hung Ta <hung.ta@eastagile.com>
2024-10-29 17:22:14 +0700

    [f] 091 - Update the route app

--- src/App.tsx
+++ src/App.tsx
@@ -15,33 +15,30 @@ import { FORGET_PASSWORD_PATH, ONBOARDING_PATH, LOGIN_PATH, RESET_PASSWORD_PATH 
 import { ResetPassword } from './components/ResetPassword'
 import { FilterProvider } from './providers/filter'
 import { LiveblocksProvider, RoomProvider } from '@liveblocks/react'
+import AuthenticatedLayout from './layouts/AuthenticatedLayout'
 
 const queryClient = new QueryClient()
 
 export default function App(): ReactElement {
   return (
     <QueryClientProvider client={queryClient}>
-      <LiveblocksProvider publicApiKey={import.meta.env.VITE_LIVEBLOCKS_PUBKEY}>
-        <RoomProvider id={import.meta.env.VITE_LIVEBLOCKS_ROOM}>
-          <AuthProvider>
-            <FilterProvider>
-              <Router>
-                <Routes>
-                  <Route path={ONBOARDING_PATH} element={<OnboardingPage />} />
-                  <Route path={LOGIN_PATH} element={<LoginPage />} />
-                  <Route path={FORGET_PASSWORD_PATH} element={<ForgetPassword />} />
-                  <Route path={RESET_PASSWORD_PATH} element={<ResetPassword />} />
-                  <Route element={<PrivateRoute />}>
-                    <Route path='/search' element={<SearchInterface />} />
-                    <Route path='/snippet/:snippetId' element={<SnippetDetail />} />
-                  </Route>
-                  <Route path='*' element={<LoginPage />} />
-                </Routes>
-              </Router>
-            </FilterProvider>
-          </AuthProvider>
-        </RoomProvider>
-      </LiveblocksProvider>
+      <AuthProvider>
+        <FilterProvider>
+          <Router>
+            <Routes>
+              <Route path={ONBOARDING_PATH} element={<OnboardingPage />} />
+              <Route path={LOGIN_PATH} element={<LoginPage />} />
+              <Route path={FORGET_PASSWORD_PATH} element={<ForgetPassword />} />
+              <Route path={RESET_PASSWORD_PATH} element={<ResetPassword />} />
+              <Route element={<AuthenticatedLayout />}>
+                <Route path='/search' element={<SearchInterface />} />
+                <Route path='/snippet/:snippetId' element={<SnippetDetail />} />
+              </Route>
+              <Route path='*' element={<LoginPage />} />
+            </Routes>
+          </Router>
+        </FilterProvider>
+      </AuthProvider>
 
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
