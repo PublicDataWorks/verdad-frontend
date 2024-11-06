786ca17216c4fef14c0d3fb7c5d1cc6d7ad396a1
Thuan Ngo <thuan.ngo@eastagile.com>
2024-11-06 14:57:44 +0700

    [f] ver161 - Fix snippet detail page layout

--- src/layouts/AuthenticatedLayout.tsx
+++ src/layouts/AuthenticatedLayout.tsx
@@ -106,7 +106,7 @@ const AuthenticatedLayout: React.FC = () => {
 
         return filteredData.map(user => user.email)
       }}>
-      <div className='flex flex-col'>
+      <div className='flex flex-col min-h-svh'>
         <HeaderBar />
         <div className='flex-grow overflow-hidden bg-ghost-white'>
           <Outlet />
