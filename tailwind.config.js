b8aa2c6a3321959ca6f7564816b6b96f2c8d7b59
Hung Ta <hung.ta@eastagile.com>
2024-12-12 14:09:16 +0700

    [f] VER-121 - Update the dark mode button and add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -142,7 +142,8 @@ const config = {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)'
-      }
+      },
+      gold: '#FFD700'
     }
   },
   plugins: [require('tailwindcss-animate')]
